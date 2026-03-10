e9272c76c63b3dfc9c147496dfe1fb04b1d529f4
hiteshbatra1 <hiteshbatra967@gmail.com>
2026-03-10 20:48:11 +0530

    feat: add blog delete functionality with user authorization

--- routes/blog.js
+++ routes/blog.js
@@ -17,10 +17,30 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+router.post("/comment/:blogId", async (req, res) => {
+  await Comment.create({
+    content: req.body.content,
+    blogId: req.params.blogId,
+    createdBy: req.user._id,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`);
+});
+
 router.get("/add-new", (req, res) => {
   return res.render("addBlog", {
     user: req.user,
   });
+});
+
+router.post("/", upload.single("coverImage"), async (req, res) => {
+  const { title, body } = req.body;
+  const blog = await Blog.create({
+    body,
+    title,
+    createdBy: req.user._id,
+    coverImageURL: `/uploads/${req.file.filename}`,
+  });
+  return res.redirect(`/blog/${blog._id}`);
 });
 
 router.get("/:id", async (req, res) => {
@@ -37,24 +57,24 @@ router.get("/:id", async (req, res) => {
   });
 });
 
-router.post("/comment/:blogId", async (req, res) => {
-  await Comment.create({
-    content: req.body.content,
-    blogId: req.params.blogId,
-    createdBy: req.user._id,
-  });
-  return res.redirect(`/blog/${req.params.blogId}`);
-});
+router.delete("/:id", async (req, res) => {
+  if (!req.user) {
+    return res.status(401).send("You must be signed in to delete a blog");
+  }
 
-router.post("/", upload.single("coverImage"), async (req, res) => {
-  const { title, body } = req.body;
-  const blog = await Blog.create({
-    body,
-    title,
-    createdBy: req.user._id,
-    coverImageURL: `/uploads/${req.file.filename}`,
-  });
-  return res.redirect(`/blog/${blog._id}`);
+  const blog = await Blog.findById(req.params.id);
+
+  if (!blog) {
+    return res.status(404).send("Blog not found");
+  }
+
+  if (blog.createdBy.toString() !== req.user._id) {
+    return res.status(403).send("You are not authorised to delete this blog");
+  }
+
+  await Blog.findByIdAndDelete(req.params.id);
+
+  return res.redirect("/");
 });
 
 module.exports = router;
